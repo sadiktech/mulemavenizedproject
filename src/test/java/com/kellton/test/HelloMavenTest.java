4abe3926cfbf408216b0b455310388f360a570de
Tigo@1357 <sadiktech@gmail.com>
2017-11-08 10:49:21 +0530

    first commit

---
+++ src/test/java/com/kellton/test/HelloMavenTest.java
@@ -0,0 +1,29 @@
+package com.kellton.test;
+
+import org.junit.Test;
+import org.mule.tck.junit4.FunctionalTestCase;
+
+public class HelloMavenTest extends FunctionalTestCase {
+
+	@Test
+    public void mavenFlowReturnsHelloMaven() throws Exception {
+        runFlowAndExpect("mavenFlow", "Hello Maven");
+    }
+	
+	   
+    @Test
+    public void retrieveFlightsFlightsMaven() throws Exception {
+        runFlowAndExpect("retrieveFlights", "Hello Sadik");
+    }
+    
+   /* @Test
+    public void logFlightsMaven() throws Exception {
+        runFlowAndExpect("logFlights", "Hello Sadik");
+    }*/
+    
+    @Override
+    protected String getConfigFile() {
+        return "mulemavenizedproject.xml";
+    }
+
+}
