c0555b76192072c44ab1e1087aa77358e4b9fdb7
Tigo@1357 <sadiktech@gmail.com>
2017-11-08 12:58:26 +0530

    MavenCorrection

--- src/test/java/com/kellton/test/HelloMavenTest.java
+++ src/test/java/com/kellton/test/HelloMavenTest.java
@@ -5,25 +5,28 @@ import org.mule.tck.junit4.FunctionalTestCase;
 
 public class HelloMavenTest extends FunctionalTestCase {
 
+	
+	 @Override
+	    protected String getConfigFile() {
+	        return "mulemavenizedproject.xml";
+	    }
+
+	 
 	@Test
     public void mavenFlowReturnsHelloMaven() throws Exception {
         runFlowAndExpect("mavenFlow", "Hello Maven");
     }
 	
 	   
-    @Test
+   /* @Test
     public void retrieveFlightsFlightsMaven() throws Exception {
         runFlowAndExpect("retrieveFlights", "Hello Sadik");
-    }
+    }*/
     
    /* @Test
     public void logFlightsMaven() throws Exception {
         runFlowAndExpect("logFlights", "Hello Sadik");
     }*/
     
-    @Override
-    protected String getConfigFile() {
-        return "mulemavenizedproject.xml";
-    }
-
+   
 }
